8b53cdcfc273b1711d369f8ac0192ea8a46a07aa
jyzhuang <jyzhuang@onlyou.com>
2018-02-12 16:04:05 +0800

    api引入

---
+++ ricky_service/src/main/java/com/ricky/user/service/UserService.java
@@ -0,0 +1,17 @@
+package com.ricky.user.service;
+
+import com.ricky.exception.BusinessException;
+import com.ricky.user.vo.UserVO;
+
+/**
+ * Describetion
+ * Created  by  zhuangjiayin
+ * Date : 2018/2/12
+ */
+public class UserService implements  IUserService {
+
+    public UserVO getUserVOBYId(String id) throws BusinessException {
+        return null;
+    }
+
+}
