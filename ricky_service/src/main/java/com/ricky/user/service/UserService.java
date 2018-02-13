32cf2ee3dafabaa473cab53a5b9c7215eceedcd0
jyzhuang <jyzhuang@onlyou.com>
2018-02-13 17:30:01 +0800

    添加方法实现

--- ricky_service/src/main/java/com/ricky/user/service/UserService.java
+++ ricky_service/src/main/java/com/ricky/user/service/UserService.java
@@ -4,23 +4,28 @@ import com.ricky.exception.BusinessException;
 import com.ricky.user.entity.UserEntity;
 import com.ricky.user.entity.UserEntityMapper;
 import com.ricky.user.vo.UserVO;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.io.InputStream;
+import javax.annotation.Resource;
+
 
 /**
  * Describetion
  * Created  by  zhuangjiayin
  * Date : 2018/2/12
  */
+@Service
 public class UserService implements  IUserService {
 
+    @Resource
+    private UserEntityMapper userEntityMapper;
+
     public UserVO getUserVOBYId(String id) throws BusinessException {
-        return null;
+        UserVO userVO=new UserVO();
+        UserEntity userEntity=   userEntityMapper.selectByPrimaryKey(id);
+        BeanUtils.copyProperties(userEntity,userVO);
+        return  userVO;
     }
 
 
