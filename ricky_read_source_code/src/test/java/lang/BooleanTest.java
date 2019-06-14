9d4cd243368e0294cf2fd96211b678b96ef66175
jyzhuang <jyzhuang@onlyou.com>
2019-06-14 18:16:36 +0800

    测试代码提交

--- ricky_read_source_code/src/test/java/lang/BooleanTest.java
+++ ricky_read_source_code/src/test/java/lang/BooleanTest.java
@@ -6,6 +6,7 @@ import junit.framework.TestCase;
  * Created by zhuangjiayin on 2019/6/9.
  */
 public class BooleanTest extends TestCase {
+
     public void testBooleanValue(){
        Boolean  a= new Boolean(true);
        Boolean  b= true;
@@ -16,6 +17,31 @@ public class BooleanTest extends TestCase {
         System.out.println(a.equals(b));
         System.out.println(a.hashCode());
         System.out.println(b.hashCode());
+    }
+
+    public void testChar(){
+
+//        Character.Subset subset=new Character.Subset("11");// 类型可见 无法编译
+//        Character.CharacterCache s=new Character.CharacterCache();//类型都不可见
+
+    }
+
+
+    public class  a {}
+
+    private   class  b {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+    private class c extends b{
+
     }
 
 }
---
+++ ricky_read_source_code/src/test/java/lang/PrivateTest.java
@@ -0,0 +1,9 @@
+package lang;
+
+/**
+ * Describetion
+ * Created  by  zhuangjiayin
+ * Date : 2019/6/14
+ */
+ class PrivateTest {
+}
